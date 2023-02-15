052a7e3168422b9870381c9eb18c38a2bbd3760e
seunghyeonfromkorea <123835505+seunghyeonfromkorea@users.noreply.github.com>
2023-02-14 21:33:20 -0500

    Add files via upload

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,52 @@
-class Conversation {
+import java.util.Scanner;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
+class Conversation {
+public static void main(String[] args) { 
+
+  String[] cannedResponses = {"Yes", "Hmmm. Please tell me more news", "Of course", "Okay", "Interesting...", "Indeed"};
+  int cannedTimes = cannedResponses.length;
+ 
+  Scanner inputRound = new Scanner(System.in);
+  System.out.println("\nHow many times do you want to talk with me?");
+  
+  int round = inputRound.nextInt();
+  inputRound.nextLine();
+
+  String[] transcript = new String[2*round+1];
+  transcript[0] = ("Hi! How are you today?"); 
+  System.out.println(transcript[0]);
+  
+    for (int i = 0; i < round; i++) {
+      String words = inputRound.nextLine();
+      String toMirror;
+
+      String newWords = words.replace("I", "you");
+      newWords = newWords.replace("me", "you");
+      newWords = newWords.replace("am", "are");
+      newWords = newWords.replace("my", "your");
+      newWords = newWords.replace("your", "my");
+      newWords = newWords.replace("my", "you");
+      
+      if (!newWords.equals(words)) {
+          toMirror = newWords;
+      } 
+      else {
+          toMirror = cannedResponses[(int) Math.floor(cannedTimes * Math.random())];
+      }
+      System.out.println(toMirror);
+      
+      transcript[2*i+1] = words;
+      transcript[2*i+2] = toMirror;
+    }
+
+  System.out.println("Good bye! See you later :)");
+
+  System.out.println(" ");
+  System.out.println("-----TRANSCRIPT-----");
+
+  for (int i = 0; i < transcript.length; i++) {
+    System.out.println(transcript[i]);
   }
+  System.exit(0);
+} 
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,23 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+
+    Seyoun, Hau, and Seunghyeon(me)
+    We discussed about what we need before we assign the code for this assignment. Every member agreed that the functions we need are loops, if statement, and input to build two scanners, the iteration for the chat's round, and change specific words into another words in our perspective like "I" into "You".
+    Two scanners would contain the information about both round(s) and message(s).
+
 - Which session(s) of TA / office hours did you attend?
+   
+   TA hour (Asher) on Monday
+   TA hour (Chris) on Tuesday
+
 - What are your initial impressions of Java? 
+
+    In contrast of Python, Java has a longer line to build a code or a function. At first, I thought that Java would be more complicated than Python because Java would need to call many functions to operate something that has a simple idea/concept.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+
+    Both Python and Java have similar functions with similar names, and I should assign every detail for one operation. The difference is a form of a function. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    It's easy to miss where we are, so we have to pay attention when we work for the loops. We need to think about the random generation of the answer to the user's input and scanners to get the user's input. Based on information from the user, our Java file(operation) would react appropriately. 
